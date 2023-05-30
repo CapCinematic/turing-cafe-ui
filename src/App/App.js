5d735d22fa894b1ae57c41d040219412ca2dfc6f
Isaiah Richardson <capcinematic@gmail.com>
2023-05-30 11:54:47 -0600

    fix/carrot on fetch calls

--- src/App/App.js
+++ src/App/App.js
@@ -6,31 +6,37 @@ class App extends Component {
   constructor(props){
     super(props);
       this.state = {
-        // reservations = []
+        reservations: []
       }
-  }
-
-  componentDidMount(){
-      fetch('<http://localhost:3001/api/v1/reservations>')
-      .then(response => response.json)
-      .then(data => console.log('log',data))
-      .then(data => this.setState({reservations : data}))
+    }
+    
+    componentDidMount(){
+      fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(data => this.setState({ reservations: data }))
       .catch(error => console.error(error))
-  }
-
-  render() {
-    return (
-      <div className="App">
+    }
+    
+    render() {
+      return (
+        <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
 
         </div>
         <div className='resy-container'>
-          
+          {this.state.reservations.map(reservation => (
+            <div key={reservation.id}>
+              <h2>{reservation.name}</h2>
+              <p>{reservation.date} at {reservation.time}</p>
+              <p>{reservation.number}</p>
+            </div>  
+          ))}
         </div>
       </div>
     )
   }
 }
+
 
 export default App;
